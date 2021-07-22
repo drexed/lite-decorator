e2cd68693d569c123d928abdf4d800e29dbead3f
Juan Gomez <j.gomez@drexed.com>
2021-07-21 22:49:23 -0400

    Update railtie

--- lib/generators/rails/decorator_generator.rb
+++ lib/generators/rails/decorator_generator.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'rails/generators'
-
 module Rails
   class DecoratorGenerator < Rails::Generators::NamedBase
 
--- lib/lite/decorator.rb
+++ lib/lite/decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+require 'generators/rails/decorator_generator' if defined?(Rails::Generators)
+
 %w[version base].each do |name|
   require "lite/decorator/#{name}"
 end
-
-require 'generators/rails/decorator_generator'
